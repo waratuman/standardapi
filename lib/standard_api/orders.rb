9badb1b758672e3f1eede8308940abbe5e48953d
Jon Bracy <jonbracy@gmail.com>
2015-10-21 10:49:01 -0700

    Support multiple orderings

--- lib/standard_api/orders.rb
+++ lib/standard_api/orders.rb
@@ -13,20 +13,27 @@ module StandardAPI
         orders.each do |key, value|
           if key.to_s.count('.') == 1
             key2, key3 = *key.to_s.split('.')
-            permitted = sanitize({key2.to_sym => { key3.to_sym => value } }, permit)
+            permitted << sanitize({key2.to_sym => { key3.to_sym => value } }, permit)
           elsif permit.include?(key.to_s)
             value = value.symbolize_keys if value.is_a?(Hash)
-            permitted = { key.to_sym => value }
+            permitted << { key.to_sym => value }
           elsif permit.find { |x| x.is_a?(Hash) && x.has_key?(key.to_s) }
             subpermit = permit.find { |x| x.is_a?(Hash) && x.has_key?(key.to_s) }[key.to_s]
             sanitized_value = sanitize(value, subpermit)
-            permitted = { key.to_sym => sanitized_value }
+            permitted << { key.to_sym => sanitized_value }
           else
             raise(ActionDispatch::ParamsParser::ParseError.new("Invalid Ordering #{orders.inspect}", nil))
           end
         end
       when Array
-        orders.each { |order| permitted << sanitize(order, permit); }
+        orders.each do |order|
+          order = sanitize(order, permit)
+          if order.is_a?(Array)
+            permitted += order
+          else
+            permitted << order
+          end
+        end
       else
 
         if orders.to_s.count('.') == 1
@@ -39,7 +46,13 @@ module StandardAPI
         end
       end
 
-      permitted
+      if permitted.is_a?(Array) && permitted.length == 1
+        permitted.first
+      else
+        permitted
+      end
+
+      # permitted
     end
 
   end
