06bd79654defdd6f7b25f57b70a08124d2d60e90
James Bracy <waratuman@gmail.com>
2021-01-04 11:10:01 -0500

    Route loading fix

--- lib/standard_api/railtie.rb
+++ lib/standard_api/railtie.rb
@@ -4,9 +4,12 @@ module StandardAPI
   class Railtie < ::Rails::Railtie
 
     initializer 'standardapi' do
+      ActiveSupport.on_load(:action_controller) do
+        ::ActionDispatch::Routing::Mapper.send :include, StandardAPI::RouteHelpers
+      end
+
       ActiveSupport.on_load(:action_view) do
         ::ActionView::Base.send :include, StandardAPI::Helpers
-        ::ActionDispatch::Routing::Mapper.send :include, StandardAPI::RouteHelpers
       end
     end
 
