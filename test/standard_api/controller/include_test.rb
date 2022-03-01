48d975a7760396c953f0c9ee9ad43df34892d617
Jon Bracy <jonbracy@gmail.com>
2022-03-01 11:04:23 -0600

    Fix test and skipping validating includes on actions that don't need it

--- test/standard_api/controller/include_test.rb
+++ test/standard_api/controller/include_test.rb
@@ -3,7 +3,7 @@ require 'standard_api/test_helper'
 class ControllerIncludesTest < ActionDispatch::IntegrationTest
 
   # = Including an invalid include
-  
+
   test "Controller#create with a valid include" do
     property = build(:property)
 
@@ -26,33 +26,34 @@ class ControllerIncludesTest < ActionDispatch::IntegrationTest
     assert_equal [photo.id], json['photos'].map { |j| j['id'] }
   end
 
-  # # test "Controller#destroy with a valid include" do
-  # # end
+  # test "Controller#destroy with a valid include" do
+  #   No body is returned on a destroy so includes are used
+  # end
 
   test "Controller#create_resource with a valid include" do
     property = create(:property, accounts: [])
     account = build(:account)
 
     post "/properties/#{property.id}/accounts", params: { account: account.attributes, include: [:photos] }, as: :json
-    
+
     json = JSON.parse(response.body)
     assert_response :created
     assert_equal [], json['photos']
   end
 
   # test "Controller#add_resource with a valid include" do
-  #   NO BODY AS OF NOW
+  #   No body is returned on a destroy so includes are used
   # end
 
   # test "Controller#remove_resource with an invalid include" do
-  #   NO BODY AS OF NOW
+  #   No body is returned on a destroy so includes are used
   # end
 
   # = Including an invalid include
-  
+
   test "Controller#create with an invalid include" do
     property = build(:property)
-    
+
     assert_no_difference 'Property.count' do
       post "/properties", params: { property: property.attributes, include: [:accounts] }, as: :json
     end
@@ -69,8 +70,9 @@ class ControllerIncludesTest < ActionDispatch::IntegrationTest
   end
 
   # test "Controller#destroy with an invalid include" do
+  #   No body is returned on a destroy so includes are used
   # end
-  
+
   test "Controller#create_resource with an invalid include" do
     property = create(:property, photos: [])
     photo = build(:photo)
@@ -79,23 +81,27 @@ class ControllerIncludesTest < ActionDispatch::IntegrationTest
     assert_equal 0, property.reload.photos.count
     assert_response :bad_request
   end
-  
+
+  # This test passes because includes are not used, response is a HEAD response
+  # and no includes are used.
   test "Controller#add_resource with an invalid include" do
     property = create(:property, photos: [])
-    photo = build(:photo)
+    photo = create(:photo)
 
     post "/properties/#{property.id}/photos/#{photo.id}", params: { include: [:camera] }, as: :json
-    assert_equal 0, property.reload.photos.count
-    assert_response :bad_request
+    assert_equal 1, property.reload.photos.count
+    assert_response :created
   end
   
+  # This test passes because includes are not used, response is a HEAD response
+  # and no includes are used.
   test "Controller#remove_resource with an invalid include" do
     photo = create(:photo)
     property = create(:property, photos: [photo])
 
     delete "/properties/#{property.id}/photos/#{photo.id}", params: { include: [:camera] }, as: :json
-    assert_equal 1, property.reload.photos.count
-    assert_response :bad_request
+    assert_equal 0, property.reload.photos.count
+    assert_response :no_content
   end
 
 end
