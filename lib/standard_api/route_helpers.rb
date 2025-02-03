3cf2bda03e4d223b67b3f1f92555ca764bcc9759
James Bracy <waratuman@Mac.localdomain>
2025-02-03 12:34:52 -0500

    More Routing

--- lib/standard_api/route_helpers.rb
+++ lib/standard_api/route_helpers.rb
@@ -24,7 +24,25 @@ module StandardAPI
       resources(*resources, options) do
         block.call if block # custom routes take precedence over standardapi routes
 
-        actions = parent_resource.actions
+        actions = if ActionPack.version < '8.0.0'
+          available_actions = if only = parent_resource.instance_variable_get(:@only)
+            Array(only).map(&:to_sym)
+          else
+            if parent_resource.instance_variable_get(:@api_only)
+              [:index, :create, :show, :update, :destroy]
+            else
+              [:index, :create, :new, :show, :update, :destroy, :edit]
+            end + [ :schema, :calculate, :add_resource, :remove_resource, :create_resource ]
+          end
+  
+          if except = parent_resource.instance_variable_get(:@except)
+            available_actions - Array(except).map(&:to_sym)
+          else
+            available_actions
+          end
+        else
+          actions = parent_resource.actions
+        end
 
         get :schema, on: :collection if actions.include?(:schema)
         get :calculate, on: :collection if actions.include?(:calculate)
